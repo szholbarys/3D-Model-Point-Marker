3a45ead23e0a15fcbcd4cc45c98aad5e294a8da0
szholbarys <sakenzholbarys@gmail.com>
2025-03-02 14:45:05 +0500

    fix

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from "three/build/three.module.js";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
